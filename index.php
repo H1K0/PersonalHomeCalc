84d1840b924d4f3df474a29e0620d39df9a95d2e
H1K0 <hiko-19@ya.ru>
2020-09-29 19:49:14 +0300

    Some design fixes

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
 	}
 	function createButton($btname,$class,$func,$title) {
 		?>
-		<div class="btn <?=$class?>" onclick="<?=$func?>;" title="<?=$title?>"><?=$btname?></div>
-		<!-- <input name="stack" type="submit" value="<?=$btname;?>" class="btn <?=$class?>" onclick="<?=$func?>;"> -->
+		<input class="btn <?=$class?>" onclick="<?=$func?>;" type="submit" value="<?=$btname;?>">
 		<?php
 	}
 ?>
@@ -21,6 +20,7 @@
 		<link rel="stylesheet" href="style.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<script src="script.js"></script>
+		<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(67775113, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/67775113" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 	</head>
 	<body>
 		<div class="calc">
